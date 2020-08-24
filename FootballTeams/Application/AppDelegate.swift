fbee596f214c455e8f2f61e3e5185532459aabdf
Nicolas Bouème <nicolasboueme@gmail.com>
2020-08-24 20:19:25 +0200

    Configure Xcode project with SwiftLint and SwiftGen.

---
+++ FootballTeams/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  FootballTeams
+//
+//  Created by Nicolas Bouème on 24/08/2020.
+//  Copyright © 2020 Nicolas Bouème. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
