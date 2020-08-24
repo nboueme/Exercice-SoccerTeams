19a45a8931c63f8bfc59921dcb5c78e5b154697b
Nicolas Bouème <nicolasboueme@gmail.com>
2020-08-24 19:59:38 +0200

    Initial Commit

---
+++ FootballTeams/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FootballTeams
+//
+//  Created by Nicolas Bouème on 24/08/2020.
+//  Copyright © 2020 Nicolas Bouème. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
